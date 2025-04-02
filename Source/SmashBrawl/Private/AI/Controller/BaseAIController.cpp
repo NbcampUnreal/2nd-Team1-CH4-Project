5a7b0f73c15632f213c7b3f01bea0181809f975d
Roanne <83761585+Roanne0258@users.noreply.github.com>
2025-04-02 22:09:15 +0900

    AI 관련 폴더 추가, 컨버팅 조금 진행

--- Source/SmashBrawl/Private/AI/Controller/BaseAIController.cpp
+++ Source/SmashBrawl/Private/AI/Controller/BaseAIController.cpp
@@ -10,14 +10,12 @@ ABaseAIController::ABaseAIController()
 void ABaseAIController::BeginPlay()
 {
     Super::BeginPlay();
-    // 초기화 등 필요한 작업 가능
 }
 
 void ABaseAIController::OnPossess(APawn* InPawn)
 {
     Super::OnPossess(InPawn);
 
-    // TODO: BehaviorTreeAsset은 블루프린트나 외부에서 설정
     if (BehaviorTreeAsset)
     {
         RunBehaviorTree(BehaviorTreeAsset);
@@ -27,5 +25,4 @@ void ABaseAIController::OnPossess(APawn* InPawn)
 
 void ABaseAIController::InitializeBlackboard()
 {
-    // Blackboard Key 초기화 등 설정
 }
--- Source/SmashBrawl/Public/AI/Character/BaseAIFighter.h
+++ Source/SmashBrawl/Public/AI/Character/BaseAIFighter.h
@@ -19,6 +19,17 @@ public:
     /** 어빌리티 관련 파라미터 설정 */
     void SetAbility(float InLeftRight, float InUpDown, int32 InParam, int32 InAbilityType, int32 InDirection);
 
+    // Target과 Parent 간 거리 체크
+    UFUNCTION(BlueprintCallable)
+    AActor* GetValidTargetByDistance(AActor* TargetActor, AActor* ParentActor, float DistanceThreshold = 400.0f);
+
+    // Target과 Parent 위치를 비교
+    UFUNCTION(BlueprintCallable)
+    bool IsTargetInRange(AActor* TargetActor, AActor* ParentActor,
+        float YMin, float YMax,
+        float ZMin, float ZMax);
+
+
 protected:
     void SetStateFromParam(int32 Param);
 
