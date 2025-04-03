b690cfc4ff337b94729f462966bc0e67d7943e83
GameNight <tmddus9908@naver.com>
2025-04-03 11:25:25 +0900

    SST 캐릭터 상속받아 Base 캐릭터 생성 후 ABP 수정하여 애니메이션 적용 확인

---
+++ Source/SmashBrawl/Private/Charactor/BaseSSTCharacter.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "Charactor/BaseSSTCharacter.h"
+
---
+++ Source/SmashBrawl/Public/Charactor/BaseSSTCharacter.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SSTCharacter.h"
+#include "BaseSSTCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMASHBRAWL_API ABaseSSTCharacter : public ASSTCharacter
+{
+	GENERATED_BODY()
+	
+};
