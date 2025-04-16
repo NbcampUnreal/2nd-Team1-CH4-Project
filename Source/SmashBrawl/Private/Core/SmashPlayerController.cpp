8742844fa157843518dc25b8742bcaabbeed424e
SeoWonHo <sjdlrnen0507@naver.com>
2025-04-16 12:54:49 +0900

    Add : Controller

--- Source/SmashBrawl/Private/Core/SmashPlayerController.cpp
+++ Source/SmashBrawl/Private/Core/SmashPlayerController.cpp
@@ -2,3 +2,26 @@
 
 
 #include "Core/SmashPlayerController.h"
+
+#include "Blueprint/UserWidget.h"
+
+ASmashPlayerController::ASmashPlayerController()
+{
+	
+}
+
+void ASmashPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (WidgetClass)
+	{
+		WidgetPtr = CreateWidget<UUserWidget>(this, WidgetClass);
+		if (WidgetPtr)
+		{
+			
+			WidgetPtr->AddToViewport();
+		}
+		
+	}
+}
